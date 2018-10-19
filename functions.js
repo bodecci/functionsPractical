563ec005a033a4f12805dbc76341922027170bc0
Bode Falade <bode.falade@outlook.com>
2018-10-19 12:58:50 -0500

    returning values for functions and console loggin those values

--- functions.js
+++ functions.js
@@ -2,5 +2,11 @@ function logger(){
   console.log('inside of logger');
 }//end of logger
 
+function returner (){
+  console.log('inside returner');
+  return true;
+}//end of returner
+
 logger();
-logger();
+returner();
+console.log('runnning returner():', returner());
